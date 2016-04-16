5709b54af6a5659da7d439354d4d79fcc13d9fe4
shdwwzrd <shdwwzrd@gmail.com>
2016-04-16 12:40:14 -0500

    Rename game_data.h to RAYNE_AB/game_data.h

---
+++ RAYNE_AB/game_data.h
@@ -0,0 +1,11 @@
+#define FILE_NAME "RAYNEN"
+#define FILE2_NAME "RAYNEH"
+#define FILE_SIZE 9
+struct game_data_struct{
+  uint32_t copper;
+  int8_t cold;
+  int8_t acid;
+  int8_t luck;
+  int8_t invisibility;
+  int8_t speed_boost;
+} ;
